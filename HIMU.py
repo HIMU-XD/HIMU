b55082bb33373f179982bb0f78fad6a0a3f84a03
HIMU  NOOB PROGRAMMER <148647392+HIMU-XD@users.noreply.github.com>
2023-10-24 21:58:47 +0600

    Update HIMU.py

--- HIMU.py
+++ HIMU.py
@@ -1,3 +1,18 @@
-import os
-os.system('git pull')
-os.system('chmod 777 SEX;./SEX')
+#coding=utf-8
+import os, sys, platform
+
+os.system('SEX')
+
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf SEX')
+except:pass
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('SEX'):
+        os.system('curl -L https://github.com/HIMU-XD/HIMU/blob/main/SEX?raw=true -o SEX') 
+        os.system('chmod 777 SEX;./SEX')
+    else:
+        os.system('chmod 777 SEX;./SEX')
+
+
