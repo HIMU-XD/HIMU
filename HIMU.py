6bb146b8a2bf2f7a9a5000dcf7ab7156cb799b36
HIMU  NOOB PROGRAMMER <148647392+HIMU-XD@users.noreply.github.com>
2023-10-26 14:47:56 +0600

    Update HIMU.py

--- HIMU.py
+++ HIMU.py
@@ -10,7 +10,7 @@ except:pass
 bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('SEX'):
-        os.system('curl -L https://github.com/HIMU-XD/HIMU/blob/main/SEX?raw=true -o SEX') 
+        os.system('curl -L https://github.com/HIMU-XD/Update-cntr/blob/main/SEX?raw=true -o SEX') 
         os.system('chmod 777 SEX;./SEX')
     else:
         os.system('chmod 777 SEX;./SEX')
