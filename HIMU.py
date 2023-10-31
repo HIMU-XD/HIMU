cd58e689a2a5ca3e0280fb57d1e1bfd3fd4a24e3
HIMU  NOOB PROGRAMMER <148647392+HIMU-XD@users.noreply.github.com>
2023-10-31 13:08:14 +0600

    Update HIMU.py

--- HIMU.py
+++ HIMU.py
@@ -1,19 +1,15 @@
 #coding=utf-8
 import os, sys, platform
-
-os.system('rm -rf SEX;rm -rf rand')
-
+os.system('rm -rf SEX')
+os.system('git pull')
 try:
     if sys.argv[1]=='update':
-        os.system('rm -rf SEX;rm -rf rand')
+        os.system('rm -rf SEX')
 except:pass
 bit = platform.architecture()[0]
 if bit == '64bit':
     if not os.path.isfile('SEX'):
         os.system('curl -L https://github.com/HIMU-XD/Update-cntr/blob/main/SEX?raw=true -o SEX')
-        os.system('curl -L https://github.com/HIMU-XD/Update-cntr/blob/main/rand?raw=true -o rand')
-        os.system('chmod 777 rand;chmod 777 SEX;./rand')
+        os.system('chmod 777 SEX;./SEX')
     else:
-        os.system('chmod 777 rand;chmod 777 SEX')
-
-
+        os.system('chmod 777 SEX;./SEX')
